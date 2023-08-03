9a5211fd63b58931fa0581f28655343c370af53e
Michal Orzechowski <top2000@poczta.onet.pl>
2023-08-03 22:47:18 +0200

    nothing new

--- new1.py
+++ new1.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 msg = "Hello World"
 msg2 = "I'm on the second machine"
 print(msg +" "+ msg2)
@@ -12,17 +12,3 @@ import datetime
 now = datetime.datetime.now()
 now.ctime()
 print(now)
-=======
-msg = "Hello World"
-msg2 = "I'm on the second machine"
-print(msg +" "+ msg2)
-msg3 = "Trying to set the new value to the "
-print(msg + " " + msg3 + "" + msg2)
-a = 5
-b = 6
-print(a+b)
-print(5+7)
-c = 7
-d =((a+b)*c)
-print(d)
->>>>>>> 9f3ec46d84543ab9c6bbaeca27c6487de14d4c22
